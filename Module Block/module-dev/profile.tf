6a7aefc601f8ef02ec1b57a7293e89dc80ae390f
Varad Gaikwad <91328424+gvarad@users.noreply.github.com>
2026-03-16 17:46:16 +0530

    Update profile.tf

--- Module Block/module-dev/profile.tf
+++ Module Block/module-dev/profile.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "eu-north-1"
-    access_key = "AKIA4RVEFHGTWKB4DJE3"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
